d21af954a242cf348a85add3b05113880b3fab91
Anton Kudryashov <driver_90@list.ru>
2024-12-07 19:03:36 +0700

    Successful result

---
+++ src/test/java/PostmanTest.java
@@ -0,0 +1,28 @@
+import io.restassured.path.json.JsonPath;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.hasSize;
+
+public class PostmanTest {
+    @Test
+    void Test() {
+        // Given - When - Then
+// Предусловия
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("some data") // отправляемые данные (заголовки и query можно выставлять аналогично)
+// Выполняемые действия
+                .when()
+                .post("/post")
+// Проверки
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("some data"))
+        ;
+
+
+    }
+
+}
