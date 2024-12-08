5a0e4e39ca252125b890f6f744ef2d1b78d64dc2
Anton Kudryashov <driver_90@list.ru>
2024-12-08 16:55:18 +0700

    With positive assembly

--- src/test/java/PostmanTest.java
+++ src/test/java/PostmanTest.java
@@ -19,7 +19,7 @@ public class PostmanTest {
 // Проверки
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("some get"))
+                .body("data", equalTo("some data"))
         ;
 
 
